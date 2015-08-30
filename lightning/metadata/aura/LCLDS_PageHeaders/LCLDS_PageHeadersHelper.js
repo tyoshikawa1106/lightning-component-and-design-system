c7cf669c6a3af6b57eae5310dfc3ae87725386ee
Taiki <tyoshikawa1106@gmail.com>
2015-08-30 19:01:12 +0900

    commit

---
+++ lightning/metadata/aura/LCLDS_PageHeaders/LCLDS_PageHeadersHelper.js
@@ -0,0 +1,141 @@
+({
+    setup: function(component) {
+        // Replace the CDATA block with svg element
+        // Workaround for bug in Aura Framework
+        
+        var svg1 = component.find("svg_content1");
+        var value1 = svg1.getElement().innerText;
+        value1 = value1.replace("", "");
+        svg1.getElement().innerHTML = value1;
+        
+        var svg2 = component.find("svg_content2");
+        var value2 = svg2.getElement().innerText;
+        value2 = value2.replace("", "");
+        svg2.getElement().innerHTML = value2;
+        
+        var svg3 = component.find("svg_content3");
+        var value3 = svg3.getElement().innerText;
+        value3 = value3.replace("", "");
+        svg3.getElement().innerHTML = value3;
+        
+        var svg4  = component.find("svg_content4");
+        var value4 = svg4.getElement().innerText;
+        value4 = value4.replace("", "");
+        svg4.getElement().innerHTML = value4;
+        
+        var svg5  = component.find("svg_content5");
+        var value5 = svg5.getElement().innerText;
+        value5 = value5.replace("", "");
+        svg5.getElement().innerHTML = value5;
+        
+        var svg6  = component.find("svg_content6");
+        var value6 = svg6.getElement().innerText;
+        value6 = value6.replace("", "");
+        svg6.getElement().innerHTML = value6;
+        
+        var svg7  = component.find("svg_content7");
+        var value7 = svg7.getElement().innerText;
+        value7 = value7.replace("", "");
+        svg7.getElement().innerHTML = value7;
+        
+        var svg8  = component.find("svg_content8");
+        var value8 = svg8.getElement().innerText;
+        value8 = value8.replace("", "");
+        svg8.getElement().innerHTML = value8;
+
+        var svg9  = component.find("svg_content9");
+        var value9 = svg9.getElement().innerText;
+        value9 = value9.replace("", "");
+        svg9.getElement().innerHTML = value9;
+
+        var svg10  = component.find("svg_content10");
+        var value10 = svg10.getElement().innerText;
+        value10 = value10.replace("", "");
+        svg10.getElement().innerHTML = value10;
+
+        var svg11  = component.find("svg_content11");
+        var value11 = svg11.getElement().innerText;
+        value11 = value11.replace("", "");
+        svg11.getElement().innerHTML = value11;
+
+        var svg12  = component.find("svg_content12");
+        var value12 = svg12.getElement().innerText;
+        value12 = value12.replace("", "");
+        svg12.getElement().innerHTML = value12;
+
+        var svg13  = component.find("svg_content13");
+        var value13 = svg13.getElement().innerText;
+        value13 = value13.replace("", "");
+        svg13.getElement().innerHTML = value13;
+
+        var svg14  = component.find("svg_content14");
+        var value14 = svg14.getElement().innerText;
+        value14 = value14.replace("", "");
+        svg14.getElement().innerHTML = value14;
+
+        var svg15  = component.find("svg_content15");
+        var value15 = svg15.getElement().innerText;
+        value15 = value15.replace("", "");
+        svg15.getElement().innerHTML = value15;
+
+        var svg16  = component.find("svg_content16");
+        var value16 = svg16.getElement().innerText;
+        value16 = value16.replace("", "");
+        svg16.getElement().innerHTML = value16;
+
+        var svg17  = component.find("svg_content17");
+        var value17 = svg17.getElement().innerText;
+        value17 = value17.replace("", "");
+        svg17.getElement().innerHTML = value17;
+
+        var svg18  = component.find("svg_content18");
+        var value18 = svg18.getElement().innerText;
+        value18 = value18.replace("", "");
+        svg18.getElement().innerHTML = value18;
+
+        var svg19  = component.find("svg_content19");
+        var value19 = svg19.getElement().innerText;
+        value19 = value19.replace("", "");
+        svg19.getElement().innerHTML = value19;
+
+        var svg20  = component.find("svg_content20");
+        var value20 = svg20.getElement().innerText;
+        value20 = value20.replace("", "");
+        svg20.getElement().innerHTML = value20;
+
+        var svg21  = component.find("svg_content21");
+        var value21 = svg21.getElement().innerText;
+        value21 = value21.replace("", "");
+        svg21.getElement().innerHTML = value21;
+
+        var svg22  = component.find("svg_content22");
+        var value22 = svg22.getElement().innerText;
+        value22 = value22.replace("", "");
+        svg22.getElement().innerHTML = value22;
+
+        var svg23  = component.find("svg_content23");
+        var value23 = svg23.getElement().innerText;
+        value23 = value23.replace("", "");
+        svg23.getElement().innerHTML = value23;
+
+        var svg24  = component.find("svg_content24");
+        var value24 = svg24.getElement().innerText;
+        value24 = value24.replace("", "");
+        svg24.getElement().innerHTML = value24;
+
+        var svg25  = component.find("svg_content25");
+        var value25 = svg25.getElement().innerText;
+        value25 = value25.replace("", "");
+        svg25.getElement().innerHTML = value25;
+
+        var svg26  = component.find("svg_content26");
+        var value26 = svg26.getElement().innerText;
+        value26 = value26.replace("", "");
+        svg26.getElement().innerHTML = value26;
+
+        var svg27  = component.find("svg_content27");
+        var value27 = svg27.getElement().innerText;
+        value27 = value27.replace("", "");
+        svg27.getElement().innerHTML = value27;
+    }
+})
